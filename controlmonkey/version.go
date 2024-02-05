963bfbec4fcb6381787b4d71d461bae46cf96460
margules <yuval@controlmonkey.io>
2024-02-05 14:37:08 +0200

    v1.6.0

--- controlmonkey/version.go
+++ controlmonkey/version.go
@@ -1,7 +1,7 @@
 package controlmonkey
 
 // SDKVersion is the current version of the SDK.
-const SDKVersion = "1.5.0"
+const SDKVersion = "1.6.0"
 
 // SDKName is the name of the SDK.
 const SDKName = "controlmonkey-sdk-go"
